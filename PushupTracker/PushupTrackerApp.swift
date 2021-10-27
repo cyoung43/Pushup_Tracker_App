9fd8b76049efacce76658e36df7584e043f59fc2
Chris Young <cyoung43@byu.edu>
2021-10-27 14:02:05 -0600

    Initial Commit

---
+++ PushupTracker/PushupTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushupTrackerApp.swift
+//  PushupTracker
+//
+//  Created by New User on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct PushupTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
